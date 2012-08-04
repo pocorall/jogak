7d44e465fbe744668e865d675a3131e0efb142a8
pocorall <pocorall@gmail.com>
2012-08-04 15:30:31 +0900

    + Viewer

--- src/main/java/net/pocorall/jogak/Boot.scala
+++ src/main/java/net/pocorall/jogak/Boot.scala
@@ -1,11 +1,11 @@
 package net.pocorall.jogak
 
-import javax.swing.{JTable, JPanel, JLabel, JFrame}
-import javax.swing.table.TableModel
-
-class ServiceDiscovery {
-
-}
+import javax.swing._
+import table.TableModel
+import java.io
+import event.TableModelListener
+import java.awt.event.{ActionEvent, ActionListener, MouseEvent, MouseAdapter}
+import java.awt.BorderLayout
 
 
 trait TreeNodeModel {
@@ -14,37 +14,91 @@ trait TreeNodeModel {
   def child(): Array[TreeNodeModel]
 }
 
-trait TreeModel[E <: TreeNodeModel] {
-  def root(): E
-}
 
 class File(file: java.io.File = new java.io.File("/")) extends TreeNodeModel {
   def parent() = new File(file.getParentFile)
 
-  def child() = {
+  def child() = file.listFiles.map(new File(_))
 
-    new File(file.getParentFile)
-  }
-
+  override def toString() = file.toString
 }
 
-class FileSystemModel extends TreeModel[File] {
+/**marker class */
+abstract class Viewer extends JPanel(new BorderLayout);
 
-}
+class TreeViewer(var model: TreeNodeModel, implicit val viewerRegistry: ViewerRegistry) extends Viewer {
+  val tableModel = new TableModel() {
+    def getRowCount = model.child().size
 
+    def getColumnCount = 1
 
-class TreeViewer[E] extends JPanel {
-  var table = new JTable;
-  add(table);
-  var model: TreeModel[E];
+    def getColumnName(columnIndex: Int) = model.toString()
 
-  def setModel(model: TreeModel[E]) {
-    this.model = model;
-    table.setModel(new TableModel() {
+    def getColumnClass(columnIndex: Int) = classOf[String]
 
-    });
+    def isCellEditable(rowIndex: Int, columnIndex: Int) = false
+
+    def getValueAt(rowIndex: Int, columnIndex: Int) = model.child()(rowIndex).toString()
+
+    def setValueAt(aValue: Any, rowIndex: Int, columnIndex: Int) {}
+
+    def addTableModelListener(l: TableModelListener) {}
+
+    def removeTableModelListener(l: TableModelListener) {}
   }
 
+  val table = new JTable(tableModel);
+
+  table.addMouseListener(new MouseAdapter() {
+    override def mouseClicked(e: MouseEvent) {
+      val p = e.getPoint
+      val row = table rowAtPoint p
+      if (row < 0) return
+      model = model.child()(row)
+      //      table.setModel(tableModel)
+      table.repaint()
+    }
+  })
+
+  val viewPanel = new JPanel()
+
+
+  private val leftPane = new JPanel(new BorderLayout)
+  private val upButton = new JButton("up")
+  upButton.addActionListener(new ActionListener() {
+    def actionPerformed(e: ActionEvent) {
+
+    }
+  })
+
+  leftPane.add(upButton, BorderLayout.NORTH)
+
+  leftPane.add(new JScrollPane(table), BorderLayout.CENTER)
+
+  private val spane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+    leftPane, new JScrollPane(viewPanel));
+
+  add(spane, BorderLayout.CENTER);
+}
+
+class BinaryFileViewer(file: java.io.File) extends Viewer {
+  add(new JLabel("Binary:" + file.toString))
+}
+
+class EverythingViewer(thing: Any) extends Viewer {
+  add(new JLabel(thing.toString))
+}
+
+trait ViewerRegistry {
+  def lookup(thing: Any): Viewer
+}
+
+class SimpleViewerRegistry extends ViewerRegistry {
+  override def lookup(thing: Any): Viewer = (thing) match {
+    case f: java.io.File if (f.isDirectory) => new TreeViewer(new File(f), this)
+    case f: java.io.File => new BinaryFileViewer(f)
+    case f => new EverythingViewer(f)
+  }
 }
 
 object Boot {
@@ -53,8 +107,9 @@ object Boot {
       def run() {
         val frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        implicit val viewerRegistry = new SimpleViewerRegistry
 
-        val label = new TreeViewer();
+        val label = new TreeViewer(new File(new io.File("C:\\")), viewerRegistry);
         frame.getContentPane().add(label);
 
         frame.pack();
